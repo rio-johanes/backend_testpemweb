029cf44155bc574f22133966d745e427cd99a2e0
MaruliRicardoSitanggang <maruli180617@gmail.com>
2025-12-05 21:27:24 +0700

    Delete routes directory

--- routes/rooms.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const roomController = require('../controllers/roomController');
-
-router.get('/', roomController.getRooms); // Public: Untuk halaman kamar.html
-router.patch('/:id/status', roomController.updateStatus); // Admin only (perlu middleware)
-
-module.exports = router;
